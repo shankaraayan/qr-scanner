766f11a12e3b9ab3351084619f77cd8cab8f1b9a
Kartik Mandothiya <kartikya27@gmail.com>
2023-05-01 11:13:05 +0530

    na

--- App.js
+++ App.js
@@ -1,10 +1,13 @@
 import { View, Text } from 'react-native'
 import React from 'react'
 
+
 export default function App() {
   return (
+    <>
     <View>
       <Text style={{textAlign:'center',fontSize:28,marginTop:10}}>Scan QR</Text>
     </View>
+    </>
   )
 }
